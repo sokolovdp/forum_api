2ae36f6199beef65ab4e8bdd8343bc6764c04f37
dp.sokolov <dp.sokolov@jet.su>
2019-09-30 21:36:44 +0300

    add routes

---
+++ api.py
@@ -0,0 +1,45 @@
+from sanic import Sanic
+from sanic.views import HTTPMethodView
+from sanic.response import json, text
+from tables import users, topics, posts, comments
+
+
+class AsyncTopicView(HTTPMethodView):
+
+    async def get(self, request):
+        return text('I am get method')
+
+    async def post(self, request):
+        return text('I am post method')
+
+    async def put(self, request):
+        return text('I am put method')
+
+    async def delete(self, request):
+        return text('I am delete method')
+
+
+class AsyncPostView(HTTPMethodView):
+
+    async def get(self, request):
+        return text('I am get method')
+
+    async def post(self, request):
+        return text('I am post method')
+
+    async def put(self, request):
+        return text('I am put method')
+
+    async def delete(self, request):
+        return text('I am delete method')
+
+
+async def create_comment(request):
+    return text('Create comment')
+
+
+
+def setup_routes(app: Sanic):
+    app.add_route(AsyncTopicView.as_view(), '/topic')
+    app.add_route(AsyncPostView.as_view(), '/post')
+    app.add_route(create_comment, '/comment', methods=['POST', 'GET'])
--- forum.py
+++ forum.py
@@ -2,14 +2,10 @@ from typing import Any
 import os
 
 from sanic import Sanic
-from sanic.log import logger
-from sanic.response import json
-
 from databases import Database
 
 from middlewares import setup_middlewares
-
-import tables
+from api import setup_routes
 
 
 def get_env_bool(name: str, default=False) -> bool:
@@ -21,8 +17,6 @@ def get_env_bool(name: str, default=False) -> bool:
 
 
 def create_sanic_app(name: str, **kwargs: Any) -> Sanic:
-    # kwargs['load_env'] = True
-    # kwargs['configure_logging'] =
     kwargs['strict_slashes'] = os.getenv('USE_STRICT_SLASHES', False)
     return Sanic(name, **kwargs)
 
@@ -43,20 +37,8 @@ def setup_database(app: Sanic):
 sanic_app = create_sanic_app('forum_api')
 
 
-async def test(request):
-    # query = tables.books.insert()
-    # values = [
-    #     {"title": "No Highway", "author": "Nevil Shute"},
-    #     {"title": "The Daffodil", "author": "SkyH. E. Bates"},
-    # ]
-    # await sanic_app.db.execute_many(query, values)
-
-    query = tables.books.select()
-    rows = await sanic_app.db.fetch_all(query)
-    return json({'books': [{row['title']: row['author']} for row in rows]})
-
 if __name__ == "__main__":
     setup_database(sanic_app)
-    sanic_app.add_route(test, '/test', methods=['GET'])
+    setup_routes(sanic_app)
     setup_middlewares(sanic_app)
     sanic_app.run(host="0.0.0.0", port=8000, debug=True, access_log=True, auto_reload=False)
