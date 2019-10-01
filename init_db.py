d21f6a778c2153664bffdb918db038d5ba3bae2d
dp.sokolov <dp.sokolov@jet.su>
2019-10-01 14:39:19 +0300

    add init_db

---
+++ init_db.py
@@ -0,0 +1,23 @@
+import os
+from sqlalchemy import create_engine
+import tables
+
+
+if __name__ == '__main__':
+
+    db_engine = create_engine(os.getenv('DATABASE_URL'), echo=True)
+    db_connection = db_engine.connect()
+
+    tables.users.create(db_connection)
+    tables.topics.create(db_connection)
+    tables.posts.create(db_connection)
+    tables.comments.create(db_connection)
+
+    query = tables.users.insert()
+    values = {
+        'login': 'admin',
+        'password': 'admin',
+        'email': 'admin@mail.ru',
+        'admin':  True
+    }
+    db_connection.execute(query, values)
