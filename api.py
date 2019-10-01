b1d7e975c2081a9e7423b358a4081e7ec78c5727
dp.sokolov <dp.sokolov@jet.su>
2019-10-01 13:17:40 +0300

    do AsyncTopicView

--- api.py
+++ api.py
@@ -3,31 +3,69 @@ from _datetime import datetime
 from sanic import Sanic
 from sanic.views import HTTPMethodView
 from sanic.response import json, text
-# from sqlalchemy_paginator import Paginator
 from tables import users, topics, posts, comments
+
+
+def row2dict(row, keys) -> dict:
+    return {key: row.get(key) for key in keys}
 
 
 class AsyncTopicView(HTTPMethodView):
 
     async def get(self, request, topic_id):
-        if int(topic_id):
-            result = 'retrive one'
+        try:
+            if int(topic_id):
+                query = topics.select().where(users.c.id == int(topic_id))
+                row = await request.app.db.fetch_one(query)
+                rows = [row, ]
+            else:
+                query = topics.select()
+                rows = await request.app.db.fetch_all(query)
+        except Exception as e:
+            return json({'error': str(e)}, status=400)
         else:
-            result = 'retrive all'
-        return text(result)
+            return json({'data': [row2dict(r, topics.columns) for r in rows]})
 
     async def post(self, request, topic_id):
-
-        return json(request.json)
+        try:
+            query = topics.insert()
+            values = {
+                'subject': request.json.get('subject'),
+                'description': request.json.get('description'),
+                'created': datetime.now(),
+                'modified': datetime.now(),
+                'user_id': 1,  # get from request.session
+            }
+            await request.app.db.execute(query, values)
+        except Exception as e:
+            return json({'error': str(e)}, status=400)
+        else:
+            return json({})
 
     async def put(self, request, topic_id):
-
-        return json(request.json)
+        try:
+            query = topics.update().where(topics.c.id == int(topic_id))
+            values = {'modified': datetime.now(), }
+            descr = request.json.get('description')
+            subj = request.json.get('subject')
+            if descr:
+                values['description'] = descr
+            if subj:
+                values['subject'] = subj
+            await request.app.db.execute(query, values)
+        except Exception as e:
+            return json({'error': str(e)}, status=400)
+        else:
+            return json({})
 
     async def delete(self, request, topic_id):
-        if not int(topic_id):
-            return json({'error': 'invalid id'}, status=400)
-        return text('I am delete method')
+        try:
+            query = topics.delete().where(topics.c.id == int(topic_id))
+            await request.app.db.execute(query)
+        except Exception as e:
+            return json({'error': str(e)}, status=400)
+        else:
+            return json({})
 
 
 class AsyncPostView(HTTPMethodView):
