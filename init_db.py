5fdfa6679e7e9a010b95c6c1ae92d71d9b71ff43
dp.sokolov <dp.sokolov@jet.su>
2019-10-01 17:04:08 +0300

    update init_db

--- init_db.py
+++ init_db.py
@@ -2,9 +2,7 @@ import os
 from sqlalchemy import create_engine
 import tables
 
-
 if __name__ == '__main__':
-
     db_engine = create_engine(os.getenv('DATABASE_URL'), echo=True)
     db_connection = db_engine.connect()
 
@@ -14,10 +12,18 @@ if __name__ == '__main__':
     tables.comments.create(db_connection)
 
     query = tables.users.insert()
-    values = {
-        'login': 'admin',
-        'password': 'admin',
-        'email': 'admin@mail.ru',
-        'admin':  True
-    }
-    db_connection.execute(query, values)
+    values = [
+        {
+            'login': 'admin',
+            'password': 'admin',
+            'email': 'admin@mail.ru',
+            'admin': True
+        },
+        {
+            'login': 'user',
+            'password': 'user',
+            'email': 'user@mail.ru',
+            'admin': False
+        },
+    ]
+    db_connection.execute_many(query, values)
