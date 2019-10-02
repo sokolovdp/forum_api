7cd66bc61940c1935ef82247334a2bd798cd4226
dp.sokolov <dp.sokolov@jet.su>
2019-10-02 21:42:43 +0300

    update init_db

--- init_db.py
+++ init_db.py
@@ -1,18 +1,29 @@
 import os
+
 from sqlalchemy import create_engine
+from sqlalchemy.exc import DBAPIError
+from psycopg2.errorcodes import DUPLICATE_TABLE
+
 import tables
 
 if __name__ == '__main__':
     database_url = os.getenv('DATABASE_URL')
+
+    print(f'creating forum api tables, database={database_url} ...')
+
     db_engine = create_engine(database_url, echo=True)
     db_connection = db_engine.connect()
-
-    print(f'creating forum api tables, database={database_url}')
-
-    tables.users.create(db_connection)
-    tables.topics.create(db_connection)
-    tables.posts.create(db_connection)
-    tables.comments.create(db_connection)
+    try:
+        tables.users.create(db_connection)
+        tables.topics.create(db_connection)
+        tables.posts.create(db_connection)
+        tables.comments.create(db_connection)
+    except DBAPIError as db_error:
+        if db_error.orig.pgcode == DUPLICATE_TABLE:
+            print('forum tables already exists!')
+            exit(0)
+        else:
+            raise db_engine
 
     query = tables.users.insert()
     values = [
@@ -30,3 +41,4 @@ if __name__ == '__main__':
         },
     ]
     db_connection.execute(query, values)
+    print('forum api tables created')
