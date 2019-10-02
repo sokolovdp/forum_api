ca2228cb088f5a6e60d7aa8161901a5a5e16a761
dp.sokolov <dp.sokolov@jet.su>
2019-10-02 13:21:27 +0300

    write tests

--- tests.py
+++ tests.py
@@ -1,32 +1,71 @@
+import os
 import json
 import unittest
+import random
+
+from sqlalchemy import create_engine
 
 from forum import app
+import tables
 
 
 class AutoRestTests(unittest.TestCase):
     """ Unit test cases for Forum Rest APIs  """
 
-    def test_create_topic(self):
+    def setUp(self):
         pass
+
+    def test_create_topic(self):
+        n = random.randint(1, 10000)
+        data = {
+            "subject": f"topic {n}",
+            "description": f"topic {n} description"
+        }
+        request, response = app.test_client.post('/topic/0', data=json.dumps(data))
+        self.assertEqual(response.status, 200)
 
     def test_create_post(self):
-        pass
+        n = random.randint(1, 10000)
+        data = {
+            "subject": f"post {n}",
+            "description": f"post {n} description"
+        }
+        request, response = app.test_client.post('topic/1/post/0', data=json.dumps(data))
+        self.assertEqual(response.status, 200)
 
     def test_create_comment(self):
-        pass
+        n = random.randint(1, 10000)
+        data = {
+            "text": f"post 1 comment {n}",
+            "comment_id": None,
+            "topic_id": 1,
+            "post_id": 2
+        }
+        request, response = app.test_client.post('/comment', data=json.dumps(data))
+        self.assertEqual(response.status, 200)
 
     def test_create_comment_for_comment(self):
-        pass
+        n = random.randint(1, 10000)
+        data = {
+            "text": f"comment for comment {n}",
+            "comment_id": 10,
+            "topic_id": 1,
+            "post_id": 2
+        }
+        request, response = app.test_client.post('/comment', data=json.dumps(data))
+        self.assertEqual(response.status, 200)
 
     def test_get_list_of_topics(self):
         request, response = app.test_client.get('/topic/0')
         self.assertEqual(response.status, 200)
         data = json.loads(response.text)
-        self.assertEqual(len(data['topics']), 2)
+        self.assertTrue(data.get('topics') is not None)
 
     def test_get_list_of_posts(self):
-        pass
+        request, response = app.test_client.get('/topic/1/post/0')
+        self.assertEqual(response.status, 200)
+        data = json.loads(response.text)
+        self.assertTrue(data.get('posts') is not None)
 
     def test_get_one_topic(self):
         request, response = app.test_client.get('/topic/1')
@@ -35,7 +74,11 @@ class AutoRestTests(unittest.TestCase):
         self.assertEqual(len(data['topics']), 1)
 
     def test_get_one_post(self):
-        pass
+        request, response = app.test_client.get('/topic/1/post/2')
+        self.assertEqual(response.status, 200)
+        data = json.loads(response.text)
+        self.assertTrue(data.get('post') is not None)
+        self.assertTrue(data.get('comments') is not None)
 
     def test_update_topic(self):
         pass
